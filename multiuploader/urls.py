a0b072ff4d6ee070ab45dd8c0513964d7b2a856a
scilla <sr.cilla@gmail.com>
2022-05-26 19:00:32 +0200

    convert names to callable views

--- multiuploader/urls.py
+++ multiuploader/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import url
+from views import *
 
 urlpatterns = [
-   url(r'^multiuploader_delete_multiple/$', 'multiuploader.views.multiuploader_delete_multiple',
-       name='multiuploader_delete_multiple'),
-   url(r'^multiuploader_delete/(?P<pk>\w+)/$', 'multiuploader.views.multiuploader_delete',
-       name='multiuploader_delete'),
-   url(r'^multiuploader/$', 'multiuploader.views.multiuploader', name='multiuploader'),
-   url(r'^multiuploader_noajax/$', 'multiuploader.views.multiuploader', kwargs={"noajax": True},
-       name='multiploader_noajax'),
-   url(r'^multiuploader_file/(?P<pk>\w*)/$', 'multiuploader.views.multi_show_uploaded',
-       name='multiuploader_file_link')
+    url(r'^multiuploader_delete_multiple/$', multiuploader_delete_multiple,
+        name='multiuploader_delete_multiple'),
+    url(r'^multiuploader_delete/(?P<pk>\w+)/$', multiuploader_delete,
+        name='multiuploader_delete'),
+    url(r'^multiuploader/$', multiuploader,
+        name='multiuploader'),
+    url(r'^multiuploader_noajax/$', multiuploader, kwargs={"noajax": True},
+        name='multiploader_noajax'),
+    url(r'^multiuploader_file/(?P<pk>\w*)/$', multi_show_uploaded,
+        name='multiuploader_file_link')
 ]
